a3658dc0da9ca564fbe7a430fd4352b71370c5b1
YasodharGubba05 <83855085+YasodharGubba05@users.noreply.github.com>
2025-01-10 21:59:08 +0530

    Update task.js

--- task.js
+++ task.js
@@ -2,4 +2,15 @@ const inputarea=document.querySelector(".inputarea")
 const button=document.querySelector(".button")
 const list=document.querySelector(".list")
 
+function inputhandler() {
+    let task=inputarea.value;
+    if(task.length==0) {
+        return;
+    }
+    const taskelem=document.createElement("li")
+    taskelem.textContent = task;
+    list.appendChild(taskelem)
+    inputarea.value=""
+}
 
+button.addEventListener("click",inputhandler)
